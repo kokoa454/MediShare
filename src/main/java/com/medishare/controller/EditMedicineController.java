87ada3137aaa66986ed141215f93d88f46c639cc
Inari Goro <qqnq7um9k@gmail.com>
2025-07-15 15:43:31 +0900

    removed an unused import

--- src/main/java/com/medishare/controller/EditMedicineController.java
+++ src/main/java/com/medishare/controller/EditMedicineController.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.medishare.model.USER_MEDICINE;
-import com.medishare.service.MedicineService;
 import com.medishare.service.UserMedicineService;
 import lombok.RequiredArgsConstructor;
 
